30af3ce8e246c2e7aef84da28d603d4f772f209a
Nawaz2000 <mail2nawaz2000@gmail.com>
2021-09-29 21:39:27 +0530

    first commit

---
+++ src/main/java/com/nawaz2000/contactmanager/ContactManagerApplication.java
@@ -0,0 +1,13 @@
+package com.nawaz2000.contactmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nawaz2000/contactmanager/ContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nawaz2000.contactmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
