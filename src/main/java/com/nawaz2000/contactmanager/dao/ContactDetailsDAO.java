9c003c29825d85fd0561ca682c58ef7e4bfcac6a
Nawaz2000 <mail2nawaz2000@gmail.com>
2021-09-30 00:01:40 +0530

    ContactDetailsDAO and UserDAO created

---
+++ src/main/java/com/nawaz2000/contactmanager/dao/ContactDetailsDAO.java
@@ -0,0 +1,9 @@
+package com.nawaz2000.contactmanager.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nawaz2000.contactmanager.entity.ContactDetails;
+
+public interface ContactDetailsDAO extends JpaRepository<ContactDetails, Integer>{
+
+}
