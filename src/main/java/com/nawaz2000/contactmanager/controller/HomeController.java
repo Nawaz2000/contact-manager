96ac42576659515d450945621b853866bf897f09
Nawaz2000 <mail2nawaz2000@gmail.com>
2021-09-29 22:10:17 +0530

    footer added

---
+++ src/main/java/com/nawaz2000/contactmanager/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.nawaz2000.contactmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String getHome() {
+		return "home";
+	}
+	
+}
