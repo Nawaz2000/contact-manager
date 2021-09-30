d574b00c59fa1bd376669f114270089aeb0632c1
Nawaz2000 <mail2nawaz2000@gmail.com>
2021-09-30 18:34:48 +0530

    contacts now can be fetched in home page

--- src/main/java/com/nawaz2000/contactmanager/controller/HomeController.java
+++ src/main/java/com/nawaz2000/contactmanager/controller/HomeController.java
@@ -1,23 +1,29 @@
 package com.nawaz2000.contactmanager.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.nawaz2000.contactmanager.dao.ContactDetailsDAO;
 import com.nawaz2000.contactmanager.dao.UserDAO;
+import com.nawaz2000.contactmanager.entity.ContactDetails;
 import com.nawaz2000.contactmanager.entity.User;
 
 @Controller
 public class HomeController {
 	
 	@Autowired
-	private UserDAO userDAO;
+	@Qualifier("contactDetailsDAO")
+	private ContactDetailsDAO contactDAO;
 	
 	@GetMapping("/")
 	public String getHome(Model model) {
-		User user = userDAO.getById(1);
-		model.addAttribute("qqq", user.getUsername());
+		List<ContactDetails> contacts = contactDAO.findAll();
+		model.addAttribute("allContacts", contacts);
 		return "home";
 	}
 	
