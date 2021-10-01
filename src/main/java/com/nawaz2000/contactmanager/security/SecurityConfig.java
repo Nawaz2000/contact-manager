383271cb8e1ab374ec964a4da4a2b5c78de272c0
Nawaz2000 <mail2nawaz2000@gmail.com>
2021-10-01 13:50:22 +0530

    registration page linked

--- src/main/java/com/nawaz2000/contactmanager/security/SecurityConfig.java
+++ src/main/java/com/nawaz2000/contactmanager/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/register").permitAll()
 		.antMatchers("/css/**").permitAll()
 		.anyRequest().authenticated()
 //		.anyRequest().permitAll()
